5b13c2c9a7ec5bbdfdd5b91fb3fe48ac7cc97bd1
khalilcharfi <khalilcharfi@users.noreply.github.com>
2016-06-24 05:24:38 +0200

    Update UploadFile.java

--- app/src/main/java/com/example/ahmed/therapiodatafordepression/ServiceThatRecordCalls/UploadFile.java
+++ app/src/main/java/com/example/ahmed/therapiodatafordepression/ServiceThatRecordCalls/UploadFile.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import java.io.File;
 
 import it.sauronsoftware.ftp4j.FTPClient;
-import it.sauronsoftware.ftp4j.FTPDataTransferListener;
 
 /**
  * Created by Khalil on 24-Jun-16.
@@ -49,54 +48,9 @@ public class UploadFile {
         String PASSWORD = userShare.getString("PASSWORD", "");
         String FTP_HOST = userShare.getString("FTP_HOST", "");*/
             Log.d("data" ,uname+" "+pw+" "+host);
-            try {
 
-                client.connect(host);
-                client.login(uname, pw);
-                client.setType(FTPClient.TYPE_AUTO);
-                client.changeDirectory("/");
 
-            /*client.createDirectory("/ahmed");
-            client.changeDirectory("/ahmed");*/
 
-                client.upload(fileName, new FTPDataTransferListener() {
-                    @Override
-                    public void started() {
-
-                    }
-
-                    @Override
-                    public void transferred(int i) {
-
-                    }
-
-                    @Override
-                    public void completed() {
-
-                    }
-
-                    @Override
-                    public void aborted() {
-
-                    }
-
-                    @Override
-                    public void failed() {
-
-                    }
-                });
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                Log.e("upload Error", e.toString());
-                //Toast.makeText(context,"client not connected !", Toast.LENGTH_LONG).show();
-                try {
-                    client.disconnect(true);
-                } catch (Exception e2) {
-                    Log.e("upload Error", e.toString());
-                    //e2.printStackTrace();
-                }
-            }
             return "Executed";
         }
 
